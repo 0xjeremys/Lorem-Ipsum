865adb171cfc5d22dd9a4cc899785549c4930a5e
simon jeremy <rukky@mbp-de-simon.home>
2023-10-30 18:23:06 +0100

    optimization section

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,15 @@ import Content from "./components/Content";
 const App = () => {
   return (
     <>
-      <section>
-        <Content
-          name="Lorem"
-          text="Lorem ipsum dolor, sit amet consedipisicing elit. Suscipit sed inventore voluptatum nostrum quisquam odit eligendi molestiae corrupti, deserunt ipsum accusamus, sit nisi autem perspiciatis."
-        />
-      </section>
+      <Content
+        name="Lorem"
+        text="Lorem ipsum dolor, sit amet consedipisicing elit. Suscipit sed inventore voluptatum nostrum quisquam odit eligendi molestiae corrupti, deserunt ipsum accusamus, sit nisi autem perspiciatis."
+      />
 
-      <section>
-        <Content
-          name="Ipsum"
-          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis error qui veniam neque doloremque suscipit vel inventore soluta repellendus aspernatur repudiandae reiciendis nulla veritatis consequuntur sequi quasi, esse, pariatur placeat rerum magnam facere sed? Saepe consequatur veritatis dicta tempora eveniet praesentium corporis facere, iure deleniti molestias velit blanditiis veniam, mollitia sunt deserunt soluta repellat optio nostrum voluptates. Voluptates, quos. Sed nihil magnam optio quia, temporibus in autem quas voluptates natus minus corrupti laboriosam voluptatibus maxime ab cum soluta a rem, pariatur rerum nisi esse consequatur suscipit aperiam! Quod, ea eius magni nihil dolores, corporis consequuntur, sequi iure eos ipsam aspernatur?"
-        />
-      </section>
+      <Content
+        name="Ipsum"
+        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis error qui veniam neque doloremque suscipit vel inventore soluta repellendus aspernatur repudiandae reiciendis nulla veritatis consequuntur sequi quasi, esse, pariatur placeat rerum magnam facere sed? Saepe consequatur veritatis dicta tempora eveniet praesentium corporis facere, iure deleniti molestias velit blanditiis veniam, mollitia sunt deserunt soluta repellat optio nostrum voluptates. Voluptates, quos. Sed nihil magnam optio quia, temporibus in autem quas voluptates natus minus corrupti laboriosam voluptatibus maxime ab cum soluta a rem, pariatur rerum nisi esse consequatur suscipit aperiam! Quod, ea eius magni nihil dolores, corporis consequuntur, sequi iure eos ipsam aspernatur?"
+      />
     </>
   );
 };
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,17 @@
 import Description from "./Description";
 import Title from "./Title";
 const Content = (props) => {
+  // return (
+  //   <>
+  //     <Title name={props.name} />
+  //     <Description text={props.text} />
+  //   </>
+  // );
   return (
-    <>
+    <section className="content-section">
       <Title name={props.name} />
       <Description text={props.text} />
-    </>
+    </section>
   );
 };
 export default Content;
